a7118b69e8557f206aff0d9d15c9045294516e7b
Edwin Rozario <edwin@boost.co.nz>
2018-02-23 12:13:44 +1300

    Fixing spacing for red down arrow

--- lib/gemstory/printer/helpers.rb
+++ lib/gemstory/printer/helpers.rb
@@ -21,7 +21,7 @@ module Gemstory
 
       def status_code
         { up: " \033[0;32m  \u2191 \033[0;m ",
-          down: " \033[0;31m   \u2193 \033[0;m ",
+          down: " \033[0;31m  \u2193 \033[0;m ",
           next: " \033[0;34m \u2192 \033[0;m " }
       end
     end
